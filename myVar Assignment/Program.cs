6482ca5f1b584bdd950f592639afc1631f4bbb57
joeycorrea4 <joeyoshi9@gmail.com>
2023-01-27 11:05:46 -0600

    Initial Commit

---
+++ myVar Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = 5;
+
+            myVar = -1 + 4 * myVar;
+            Console.WriteLine(myVar);
+
+            myVar = (35 + 5) % myVar;
+            Console.WriteLine(myVar);
+
+            myVar = 14 + -4 * 6 / 12;
+            Console.WriteLine(myVar);
+
+            myVar = 2 + 12 / 6 * 1 - myVar % 2;
+            Console.WriteLine(myVar);
+
+            myVar = myVar * myVar;
+            {
+                if (myVar < 10)
+                    Console.WriteLine("myVar is less than 4!");
+                else
+                    Console.WriteLine("myVar is greater than 4!");
+            }
+        }
+    }
+}
